dc34f53e1b787b06fd12b266f68386e3c6c37c1c
Paul Cullender <paul@pkhome.co.uk>
2017-08-02 08:17:56 +0100

    change release task

---
+++ dist/strokeList.js
@@ -0,0 +1,22 @@
+var strokeTable = {
+    '50 Freestyle': '50 Free',
+    '100 Freestyle': '100 Free',
+    '200 Freestyle': '200 Free',
+    '400 Freestyle': '400 Free',
+    '800 Freestyle': '800 Free',
+    '1500 Freestyle': '1500 Free',
+    '50 Breaststroke': '50 Breast',
+    '100 Breaststroke': '100 Breast',
+    '200 Breaststroke': '200 Breast',
+    '50 Butterfly': '50 Fly',
+    '100 Butterfly': '100 Fly',
+    '200 Butterfly': '200 Fly',
+    '50 Backstroke': '50 Back',
+    '100 Backstroke': '100 Back',
+    '200 Backstroke': '200 Back',
+    '100 Individual Medley': '100 IM',
+    '200 Individual Medley': '200 IM',
+    '400 Individual Medley': '400 IM'
+};
+
+module.exports = strokeTable;
